b8a24c52b4cfeda57f6b5e7e0ebaa9fce53311d4
Ramish Saeed <rsaeed.bese20seecs@seecs.edu.pk>
2025-03-17 00:42:44 +0500

    created home screen ui

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -1,19 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:nustea/controller/auth_controller.dart';
+import 'package:nustea/features/home/drawers/community_list_drawer.dart';
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
+
+  void displayDrawer(BuildContext context) {
+    Scaffold.of(context).openDrawer();
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final user = ref.watch(userProvider);
     return Scaffold(
-      body: Center(
-        child: Text(
-          user?.name ?? '',
-        ),
+      appBar: AppBar(
+        title: const Text('Home'),
+        centerTitle: false,
+        leading: Builder(builder: (context) {
+          return IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () => displayDrawer(context),
+          );
+        }),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search),
+          ),
+          IconButton(
+            icon: CircleAvatar(
+              backgroundImage: NetworkImage(user!.profilePic),
+            ),
+            onPressed: () {},
+          )
+        ],
       ),
+      drawer: const CommunityListDrawer(),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:nustea/controller/auth_controller.dart';
 import 'package:nustea/core/common/error_text.dart';
 import 'package:nustea/core/common/loader.dart';
+import 'package:nustea/models/user_model.dart';
 import 'package:nustea/router.dart';
 import 'package:nustea/theme/pallete.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:routemaster/routemaster.dart';
 import 'firebase_options.dart';
-
-// ...
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,12 +23,27 @@ void main() async {
   );
 }
 
-class MyApp extends ConsumerWidget {
+class MyApp extends ConsumerStatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ConsumerStatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends ConsumerState<MyApp> {
+  UserModel? userModel;
+
+  void getData(WidgetRef ref, User data) async {
+    userModel = await ref
+        .watch(authControllerProvider.notifier)
+        .getUserData(data.uid)
+        .first;
+    ref.read(userProvider.notifier).update((state) => userModel);
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return ref.watch(authStateChangeProvider).when(
           data: (data) => MaterialApp.router(
             debugShowCheckedModeBanner: false,
@@ -37,7 +52,10 @@ class MyApp extends ConsumerWidget {
             routerDelegate: RoutemasterDelegate(
               routesBuilder: (context) {
                 if (data != null) {
-                  return loggedInRoute;
+                  getData(ref, data);
+                  if (userModel != null) {
+                    return loggedInRoute;
+                  }
                 }
                 return loggedOutRoute;
               },
