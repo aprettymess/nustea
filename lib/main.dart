254ec687832d0fa7dfa7d982b6a4f4f39d879d1f
a-arham-x <108538325+a-arham-x@users.noreply.github.com>
2025-03-16 22:04:25 +0500

    backend conf

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:nustea/features/auth/screens/login_screen.dart';
 import 'package:nustea/theme/pallete.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+// ...
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
